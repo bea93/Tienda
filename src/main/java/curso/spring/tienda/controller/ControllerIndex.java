ab91bf4f4435b304ccdbfc22c4c0d4bc5a15581d
bea93 <bmerinomacia@gmail.com>
2021-11-04 23:59:40 +0100

    Primer commit en develop

---
+++ src/main/java/curso/spring/tienda/controller/ControllerIndex.java
@@ -0,0 +1,13 @@
+package curso.spring.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ControllerIndex {
+	
+	
+}
---
+++ src/main/java/curso/spring/tienda/model/Cathalog.java
@@ -0,0 +1,33 @@
+package curso.spring.tienda.model;
+
+import java.util.ArrayList;
+
+public class Cathalog {
+	private static ArrayList<Product> catalogo = cargarCatalogo();
+	
+	private static ArrayList<Product> cargarCatalogo(){
+		ArrayList<Product> catalogo = new ArrayList<Product>();
+		catalogo.add(new Product(1, "Cerveza", 1.0));
+		catalogo.add(new Product(2, "Macarrones", 1.8));
+		catalogo.add(new Product(3, "Queso", 2.5));
+		catalogo.add(new Product(4, "Chocolate", 3.0));
+		catalogo.add(new Product(5, "Galletas", 4.30));
+		
+		return catalogo;
+	}
+	
+	public static ArrayList<Product> getCatalogo(){
+		return catalogo;
+	}
+	
+	public static Product getProductoById(Integer id) {
+		for(int i = 0; i < catalogo.size(); i++) {
+			Product producto = catalogo.get(i);
+			if(producto.getId() == id) {
+				return producto;
+			}
+		}
+		
+		return null;
+	}
+}
